665b67e75164cf2477f15b5726175fb0d852fa71
Max <120524043+Comrade-Bacon@users.noreply.github.com>
2023-02-05 16:22:45 -0700

    added shooting cool down; added background; added guts to change max score; shooting now destroys the other ship

--- main.ts
+++ main.ts
@@ -5,66 +5,42 @@
 Game set up
 */
 
+// set up new sprite kinds
+namespace SpriteKind {
+    export const Player2 = SpriteKind.create() // set up player 2 sprite kind
+    export const Player1 = SpriteKind.create() // set up player 1 sprite kind
+}
+
+
 // Global variables
 
 let isPlyin = false
 let maxScore = 5
+
 let p1ReadyToFire = true
 let p2ReadyToFire = true
+
+let p1Bullet: Sprite = null
+let p2Bullet: Sprite = null
 
 // finction for seting up the player sprites
 function plrSetUp(player: number){ 
     if (player == 1) {
-        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
-    . . . . . . . . . . f 
-    . . . . . . . . . . 1 
-    . . . 1 8 8 2 2 1 1 1 
-    5 5 5 1 8 8 1 1 1 1 1 
-    5 5 5 1 1 1 1 1 1 1 1 
-    . . . 1 2 2 2 2 1 1 1 
-    . . . . . . . . . . 1 
-    . . . . . . . . . . f 
-    `, SpriteKind.Player))
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`p1Sprite`, SpriteKind.Player1))
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setPosition(152, 60)
         mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 0, 100)
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setStayInScreen(true)
 
     } else if (player == 2) {
-        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
-    . . . . . . 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . 
-    . . 2 2 2 . 2 2 . 2 2 2 . . 
-    . 2 2 . 2 . 2 2 . 2 . 2 2 . 
-    2 2 2 . 2 . 2 2 . 2 . 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 . . 2 2 . . 2 2 2 . 
-    . . 2 . . . . . . . . 2 . . 
-    `, SpriteKind.Player))
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`p2Sprite`, SpriteKind.Player2))
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setPosition(10, 60)
         mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), 0, 100)
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setStayInScreen(true)
     
     } else {
-        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
-    . . . . . . . . . . f 
-    . . . . . . . . . . 1 
-    . . . 1 8 8 2 2 1 1 1 
-    5 5 5 1 8 8 1 1 1 1 1 
-    5 5 5 1 1 1 1 1 1 1 1 
-    . . . 1 2 2 2 2 1 1 1 
-    . . . . . . . . . . 1 
-    . . . . . . . . . . f 
-    `, SpriteKind.Player))
-        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
-    . . . . . . 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 . . . . 
-    . . 2 2 2 . 2 2 . 2 2 2 . . 
-    . 2 2 . 2 . 2 2 . 2 . 2 2 . 
-    2 2 2 . 2 . 2 2 . 2 . 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    . 2 2 2 . . 2 2 . . 2 2 2 . 
-    . . 2 . . . . . . . . 2 . . 
-    `, SpriteKind.Player))
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`p1Sprite`, SpriteKind.Player1))
+        mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`p2Sprite`, SpriteKind.Player2))
+        
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setPosition(152, 60)
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setPosition(10, 60)
 
@@ -104,6 +80,9 @@ pauseUntil(() => isPlyin == true) // wait untill player 1 selects play
 start game
 */
 
+scene.setBackgroundImage(assets.image`backGround`)
+
+
 plrSetUp(0) // spawn both ships
 
 
@@ -112,7 +91,7 @@ mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function
 
 if (player.getProperty(mp.PlayerProperty.Number) == 1 && p1ReadyToFire == true) { // if player one shot and cooldown is not active
     
-    let p1Bullet = sprites.createProjectileFromSprite(assets.image`p1Shot`,mp.getPlayerSprite(player) , -100, 0) // shoot a projectile from player 1
+    p1Bullet = sprites.createProjectileFromSprite(assets.image`p1Shot`,mp.getPlayerSprite(player) , -100, 0) // shoot a projectile from player 1
     p1Bullet.setFlag(SpriteFlag.AutoDestroy, true) // atomaticly destroy projectile onc it leaves the screen
     
     //shot cooldown
@@ -123,7 +102,7 @@ if (player.getProperty(mp.PlayerProperty.Number) == 1 && p1ReadyToFire == true) 
 
 } else if (player.getProperty(mp.PlayerProperty.Number) == 2 && p2ReadyToFire == true){ // if player 2 shot and cooldown is not active
     
-    let p2Bullet = sprites.createProjectileFromSprite(assets.image`p2Shot`, mp.getPlayerSprite(player), 100, 0) // shoot a projectile from player 2
+    p2Bullet = sprites.createProjectileFromSprite(assets.image`p2Shot`, mp.getPlayerSprite(player), 100, 0) // shoot a projectile from player 2
     p2Bullet.setFlag(SpriteFlag.AutoDestroy, true) // atomaticly destroy projectile onc it leaves the screen
     
     // shot cooldown
@@ -135,3 +114,20 @@ if (player.getProperty(mp.PlayerProperty.Number) == 1 && p1ReadyToFire == true) 
 }
 
 })
+
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player2, function(sprite: Sprite, otherSprite: Sprite) {
+    if (!(sprite == p2Bullet)) {
+    sprite.destroy()
+    otherSprite.destroy(effects.fire, 100)   
+
+}
+})
+
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player1, function (sprite: Sprite, otherSprite: Sprite) {
+    if (!(sprite == p1Bullet)) {
+        sprite.destroy()
+        otherSprite.destroy(effects.fire, 100)
+
+    }
+})
+
